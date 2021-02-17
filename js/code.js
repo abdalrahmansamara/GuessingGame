d001017534f5293240bd3b16ec730b9161727cfa
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-02-17 18:51:20 +0200

    GuessingGame with improvements[arrays and functions]

--- js/code.js
+++ js/code.js
@@ -1,12 +1,9 @@
-function greating (){
-  let userName = prompt('whats your name?');
- alert('Welcome to this website '+ userName);}
+'use script';
+let userName = prompt('whats your name?');
+function greating(){
+  alert('Welcome to this website '+ userName);}
 greating();
 let question1 = '';
-let question2 = '';
-let question3 = '';
-let question4 = '';
-let question5 = '';
 let number;
 let question6Condition = false;
 let question7Condition = false;
@@ -14,79 +11,31 @@ let letterGuess = '';
 let cond = true;
 let a = 0;
 let favouriteLetter = ['f','w','p'];
-for(;cond;)
+let x = ['Do you think i am a cat person? y/n yes/no','Do you think i am rich? y/n yes/no','Do you think i eat healthy y/n yes/no','Do you think i work for google? y/n yes/no','Do you think i sleep well? y/n yes/no'];
+let y = ['yes','no','y','n'];
+let z = ['Thats correct','you are wrong'];
+function questions(num){
+
+  for(;cond;) {
+    question1 = prompt(x[num]).toLowerCase();
+    if(question1 === y[0] || question1 === y[1] || question1 === y[2] || question1 === y[3])
+    {
+      break;
+    }
+  }
+  if(question1 === y[0] || question1 === y[2]){
+    console.log(z[0]);
+    alert(z[0]);
+    a++;
+  }
+  else {
+    console.log(z[1]);
+    alert(z[1]);
+  }
+}
+for(let p = 0; p<=4;p++)
 {
-  question1 = prompt('Do you think i am a cat person? y/n yes/no').toLowerCase();
-  if(question1 === 'yes' || question1 === 'no' || question1 === 'y' || question1 === 'n')
-  {break;}
-}
-if(question1 === 'yes' || question1 === 'y'){
-  console.log('i am a cat person indeed');
-  alert('i am a cat person indeed');
-  a++;
-}
-else {
-  console.log('you guessed wrong');
-  alert('you guessed worng');
-}
-while(cond)
-{
-  question2 = prompt('Do you think i am rich? y/n yes/no').toLowerCase();
-  if(question2 === 'yes' || question2 === 'no' || question2 === 'y' || question2 === 'n')
-  {break;}
-}
-if(question2 === 'yes' || question2 === 'y'){
-  console.log('i am doing very good actually');
-  alert('i am doing very good actually');
-  a+= 1;}
-else {
-  console.log('you guessed wrong');
-  alert('you guessed worng');
-}
-while(cond)
-{
-  question3 = prompt('Do you think i eat healthy y/n yes/no').toLowerCase();
-  if(question3 === 'yes' || question3 === 'no' || question3 === 'y' || question3 === 'n')
-  {break;}
-}
-if(question3 === 'yes' || question3 === 'y'){
-  console.log('thats correct, i eat healthy');
-  alert('thats correct, i eat healthy');
-  a+= 1;
-}
-else {
-  console.log('you guessed wrong');
-  alert('you guessed worng');
-}
-while(cond)
-{
-  question4 = prompt('Do you think i work for google? y/n yes/no').toLowerCase();
-  if(question4 === 'yes' || question4 === 'no' || question4 === 'y' || question4 === 'n')
-  {break;}
-}
-if(question4 === 'yes' || question4 === 'y'){
-  console.log('thats correct, i work for google');
-  alert('thats correct, i work for google');
-  a+= 1;
-}
-else {
-  console.log('you guessed wrong');
-  alert('you guessed worng');
-}
-while(cond)
-{
-  question5 = prompt('Do you think i sleep well? y/n yes/no').toLowerCase();
-  if(question5 === 'yes' || question5 === 'no' || question5 === 'y' || question5 === 'n')
-  {break;}
-}
-if(question5 === 'yes' || question5 === 'y'){
-  console.log('thats not correct, i barely sleep');
-  alert('thats not correct, i barely sleep');
-}
-else {
-  console.log('thats correct, i dont have sufficient time to sleep');
-  alert('thats correct, i dont have sufficient time to sleep');
-  a+=1;
+  questions(p);
 }
 for(let i = 0;i<4;i++)
 {
