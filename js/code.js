67647165ed82c9fc39d57c5d7225cb49a9af3f1d
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-02-18 05:09:24 +0200

    repair some questions

--- js/code.js
+++ js/code.js
@@ -1,6 +1,6 @@
 'use script';
-let userName = prompt('whats your name?');
-function greating(){
+let userName = prompt( 'whats your name?' );
+function greating() {
   alert('Welcome to this website '+ userName);}
 greating();
 let question1 = '';
@@ -10,83 +10,83 @@ let question7Condition = false;
 let letterGuess = '';
 let cond = true;
 let a = 0;
-let favouriteLetter = ['f','w','p'];
-let x = ['Do you think i am a cat person? y/n yes/no','Do you think i am rich? y/n yes/no','Do you think i eat healthy y/n yes/no','Do you think i work for google? y/n yes/no','Do you think i sleep well? y/n yes/no'];
-let y = ['yes','no','y','n'];
-let z = ['Thats correct','you are wrong'];
-function questions(num){
+let favouriteLetter = [ 'f' , 'w' , 'p' ];
+let x = [ 'Do you think i am a cat person? y/n yes/no' , 'Do you think i am rich? y/n yes/no' , 'Do you think i eat healthy y/n yes/no' , 'Do you think i work for google? y/n yes/no' , 'Do you think i sleep well? y/n yes/no' ];
+let y = [ 'yes' ,'no' , 'y' , 'n' ];
+let z = [ 'i am a cat person indeed' , 'i am doing very good actually' , 'thats correct, i eat healthy' , 'thats correct, i work for google' , 'thats correct, i do have sufficient time to sleep, it\'s 201', 'you guessed worng' ];
+function questions( num ){
 
-  for(;cond;) {
-    question1 = prompt(x[num]).toLowerCase();
+  for( ; cond ; ) {
+    question1 = prompt( x[num] ).toLowerCase();
     if(question1 === y[0] || question1 === y[1] || question1 === y[2] || question1 === y[3])
     {
       break;
     }
   }
   if(question1 === y[0] || question1 === y[2]){
-    console.log(z[0]);
-    alert(z[0]);
+    console.log( z[num]) ;
+    alert( z[num] );
     a++;
   }
   else {
-    console.log(z[1]);
-    alert(z[1]);
+    console.log( z[5] );
+    alert( z[5]) ;
   }
 }
-for(let p = 0; p<=4;p++)
+for( let p = 0 ; p<=4 ; p++ )
 {
-  questions(p);
+  questions( p );
 }
-for(let i = 0;i<4;i++)
+for(let i = 0; i<4 ; i++ )
 {
-  number = prompt('please Enter a number between 0 and a 100');
-  for(;;)
+  number = prompt( 'please Enter a number between 0 and a 100' );
+  for( ; ; )
   {
-    if(isNaN(number)) //3
+    if( isNaN( number )) //3
     {
-      alert('this is not a numeric vale, please enter a number');
-      number = prompt('please Enter a number between 0 and a 100');
+      alert( 'this is not a numeric vale, please enter a number' );
+      number = prompt( 'please Enter a number between 0 and a 100' );
     }
     else{
       break;
     }
   }
-  if(number>36)
-  {alert('too high');}
-  else if (number < 36)
-  {alert('too low');}
+  if( number>36 )
+  {alert( 'too high' );}
+  else if ( number < 36 )
+  {alert( 'too low' );}
   else
   {
     a++;
     question6Condition = true;
-    alert('you Guessed the number correctly, congrats!!!');
+    alert( 'you Guessed the number correctly, congrats!!!' );
     break;
   }
 }
-if(question6Condition === false)
+if( question6Condition === false )
 {
-  alert('Sorry, the number was 36');
+  alert( 'Sorry, the number was 36' );
 }
-for(let j = 0; j < 6; j++)
+for( let j = 0; j < 6; j++ )
 {
-  letterGuess = prompt('i have three favourite letters, can you gess any of them? ').toLowerCase();
-  for(let k = 0; k<=2;k++)
+  letterGuess = prompt( 'i have three favourite letters, can you gess any of them? ' ).toLowerCase();
+  for( let k = 0 ; k<=2 ; k++ )
   {
-    if(letterGuess === favouriteLetter[k])
+    if( letterGuess === favouriteLetter[k] )
     {
       a++;
       question7Condition = true;
-      alert('congrats, you Guessed correctly, the correct answers were '+ favouriteLetter[0] + ' ,' + favouriteLetter[1]+ ' and '+ favouriteLetter[2]);
+      alert( 'congrats, you Guessed correctly, the correct answers were ' + favouriteLetter[0] + ' ,' + favouriteLetter[1]+ ' and '+ favouriteLetter[2]);
       break;
     }
   }
-  if(question7Condition === true)
+  if( question7Condition === true )
   {break;}
-  alert('Sorry, try again');
+  alert( 'Sorry, try again' );
 }
-if(question7Condition === false)
+if( question7Condition === false )
 {
-  alert('you guessed wrong, the correct answers were '+ favouriteLetter[0] + ' ,' + favouriteLetter[1]+ ' and '+ favouriteLetter[2]);
+  alert( 'you guessed wrong, the correct answers were ' + favouriteLetter[0] + ' ,' + favouriteLetter[1]+ ' and '+ favouriteLetter[2]);
 }
-alert('your overall score is '+ a +'/7');
-alert('i want to welcome you again ' + userName);
+alert( 'your overall score is ' + a + '/7' );
+alert( 'i want to welcome you again ' + userName);
